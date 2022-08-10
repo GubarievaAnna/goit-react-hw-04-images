51b009595e191327e5bc96e095e08dff21a0bb41
GubarievaAnna <annagubarieva@gmail.com>
2022-08-10 18:15:59 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../utils/api';
 import Button from '../Button/Button.jsx';
@@ -14,30 +14,27 @@ const ImageGallery = ({ keyWord }) => {
   const [isLoadMore, setIsLoadMore] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchPhotos = useCallback(
-    currentPage => {
-      setIsLoadMore(false);
-      setIsLoading(true);
-      api(keyWord, currentPage)
-        .then(({ hits, totalHits }) => {
-          if (hits.length === 0) {
-            setStatus('rejected');
-            return;
-          }
-          setImages(prev => [...prev, ...hits]);
-          setStatus('resolved');
-          Math.ceil(totalHits / 12) === page
-            ? setIsLoadMore(false)
-            : setIsLoadMore(true);
-        })
-        .catch(error => {
-          setStatus('error');
-          setError(error);
-        })
-        .finally(() => setIsLoading(false));
-    },
-    [keyWord, page]
-  );
+  const fetchPhotos = currentPage => {
+    setIsLoadMore(false);
+    setIsLoading(true);
+    api(keyWord, currentPage)
+      .then(({ hits, totalHits }) => {
+        if (hits.length === 0) {
+          setStatus('rejected');
+          return;
+        }
+        setImages(prev => [...prev, ...hits]);
+        setStatus('resolved');
+        Math.ceil(totalHits / 12) === page
+          ? setIsLoadMore(false)
+          : setIsLoadMore(true);
+      })
+      .catch(error => {
+        setStatus('error');
+        setError(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
   useEffect(() => {
     if (keyWord === '') return;
@@ -45,12 +42,14 @@ const ImageGallery = ({ keyWord }) => {
     setStatus('');
     setImages([]);
     fetchPhotos(1);
-  }, [keyWord, fetchPhotos]);
+    // eslint-disable-next-line
+  }, [keyWord]);
 
   useEffect(() => {
     if (page === 1) return;
     fetchPhotos(page);
-  }, [page, fetchPhotos]);
+    // eslint-disable-next-line
+  }, [page]);
 
   const changePageQuery = () => setPage(prev => prev + 1);
 
